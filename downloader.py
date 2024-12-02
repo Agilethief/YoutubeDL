d6772b654713bc1e2b4fcdedbff34a1791d88d2f
Agilethief <tbermanseder@gmail.com>
2024-12-02 22:09:49 +1100

    Started work on this POC

---
+++ downloader.py
@@ -0,0 +1,45 @@
+import yt_dlp
+
+
+def main():
+    yt_url = "https://www.youtube.com/watch?v=8OAPLk20epo"
+    # yt_url = "https://www.youtube.com/playlist?list=PLFBCBEdrXs4M1JOLUEwygABKBQAsUQjL0"
+    # yt_url = "https://www.youtube.com/@SMWCustomSongs/videos"
+    download_audio(yt_url)
+
+
+def download_audio(yt_url):
+    ydl_opts = {
+        "verbose": True,
+        "outtmpl": "downloads" + "/%(title)s.%(ext)s",  # Save path and file name
+        "format": "bestaudio/best",  # Get the best audio or fallback to best format
+        "postprocessor_args": {
+            "ffmpeg": ["-y"],  # Force overwrite without locking
+        },
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",  # Extract audio from video
+                "preferredcodec": "m4a",  # Convert to m4a
+                "preferredquality": "0",  # '0' means best quality
+            },
+            {
+                "key": "FFmpegMetadata",  # Embed metadata (e.g., title, artist)
+            },
+            {
+                "key": "EmbedThumbnail",  # Embed thumbnail into the audio file
+                "already_have_thumbnail": False,  # Download thumbnail if not already available
+            },
+        ],
+        "writethumbnail": True,  # Ensure thumbnail is downloaded
+        "addmetadata": True,  # Ensure metadata is added
+        "merge_output_format": "m4a",  # Final output format
+        "windowsfilenames": True,  # Ensure Windows-compatible filenames
+        "nooverwrites": True,  # Prevent overwriting
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([yt_url])
+
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# This is a simple proof of concept tool for downloading music
+
+based one: https://www.freecodecamp.org/news/download-trim-mp3-from-youtube-with-python/
